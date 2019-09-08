5b73bdd14c9267af96783a7ed5a4db81b486b451
1579502635@qq.com <1992720jyl>
2019-09-08 18:06:47 +0800

    wide and deep estimator init

--- WideAndDeep/WideAndDeepNeuralNet.py
+++ WideAndDeep/WideAndDeepNeuralNet.py
@@ -2,24 +2,18 @@
 
 import os
 import gc
-import importlib
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tqdm import tqdm
 from scipy.special import logit, expit
 from sklearn.metrics import roc_auc_score
-from category_encoders import BinaryEncoder
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import StratifiedKFold
 np.random.seed(7)
 tf.random.set_seed(7)
 pd.set_option("max_row", None)
 pd.set_option("max_columns", None)
-
-
-def auc_score(y_true, y_pred):
-    return tf.py_function(roc_auc_score, (y_true, y_pred), tf.double)
 
 
 class WideAndDeepNeuralNet(object):
@@ -32,15 +26,18 @@ class WideAndDeepNeuralNet(object):
         self.__test_feature, self.__test_index = [None for _ in range(2)]  # test_index dataframe
 
         self.__columns = list()
-        self.__columns_counts = dict()  # each fold clear
+        self.__columns_counts = dict()
 
-        # model fit predict
+        # blending
         self.__folds = None
         self.__val_preds = None
         self.__sub_preds = None
 
-        self.__neural_net_util = importlib.import_module("NeuralNetUtil")
-        self.__net, self.__early_stopping = [None for _ in range(2)]
+        # model
+        self.__wide_columns = []
+        self.__deep_columns = []
+
+        self.__model = None
 
     def data_read(self):
         self.__train = pd.read_csv(os.path.join(self.__input_path, "train.csv"))
@@ -105,63 +102,64 @@ class WideAndDeepNeuralNet(object):
 
                     encoder = LabelEncoder()
                     encoder.fit(trn_x[col])
-                    trn_x[col] = encoder.transform(trn_x[col])
-                    val_x[col] = encoder.transform(val_x[col])
-                    tes_x[col] = encoder.transform(tes_x[col])
+                    trn_x[col] = encoder.transform(trn_x[col]).astype(str)
+                    val_x[col] = encoder.transform(val_x[col]).astype(str)
+                    tes_x[col] = encoder.transform(tes_x[col]).astype(str)
 
-                    self.__columns_counts[col] = len(encoder.classes_)
+                    self.__columns_counts[col] = encoder.classes_.tolist()
 
-            trn_feature_for_model = []
-            val_feature_for_model = []
-            tes_feature_for_model = []
-
-            for col in self.__columns_counts.keys():
-                trn_feature_for_model.append(trn_x[col].values)
-                val_feature_for_model.append(val_x[col].values)
-                tes_feature_for_model.append(tes_x[col].values)
-
-            self.__net = self.__neural_net_util.network(
-                columns_counts=self.__columns_counts,
-                output_layer_bias=trn_y.mean()
-            )
-            self.__net.compile(
-                loss="binary_crossentropy", optimizer=tf.keras.optimizers.Adam(), metrics=[auc_score])
-
-            self.__net.fit(
-                x=trn_feature_for_model,
-                y=trn_y.values,
-                epochs=75,
+            trn_input = tf.compat.v1.estimator.inputs.pandas_input_fn(
+                x=trn_x,
+                y=trn_y,
                 batch_size=512,
-                verbose=2,
-                callbacks=[
-                    tf.keras.callbacks.EarlyStopping(
-                        patience=5,
-                        restore_best_weights=True
-                    )],
-                validation_data=(val_feature_for_model, val_y.values)
+                num_epochs=1,
+                shuffle=True
+            )
+            val_input = tf.compat.v1.estimator.inputs.pandas_input_fn(
+                x=val_x,
+                batch_size=val_x.shape[0],
+                shuffle=False
+            )
+            tes_input = tf.compat.v1.estimator.inputs.pandas_input_fn(
+                x=tes_x,
+                batch_size=tes_x.shape[0],
+                shuffle=False
             )
 
-            self.__neural_net_util.network_preformance(
-                n_fold=n_fold,
-                net=self.__net,
-                trn_feature=trn_feature_for_model,
-                val_feature=val_feature_for_model,
-                trn_label=trn_y,
-                val_label=val_y
+            for column, count in self.__columns_counts.items():
+                categorical_column = tf.feature_column.categorical_column_with_vocabulary_list(
+                    key=column, vocabulary_list=count)
+
+                self.__wide_columns.append(tf.feature_column.indicator_column(categorical_column))
+                self.__deep_columns.append(tf.feature_column.embedding_column(
+                    categorical_column, dimension=4 * int(np.round(np.log2(len(count))))))
+
+            self.__model = tf.estimator.DNNLinearCombinedClassifier(
+                linear_feature_columns=self.__wide_columns,
+                dnn_feature_columns=self.__deep_columns,
+                dnn_hidden_units=[1],
+                dnn_activation_fn=tf.nn.sigmoid
             )
+            self.__model.train(input_fn=trn_input)
 
-            pred_vals = self.__net.predict(val_feature_for_model).reshape((-1,))  # 2D shape -> 1D shape
-            self.__val_preds[val_idx] += logit(pred_vals)
+            pred_val, pred_tes = [np.array([]) for _ in range(2)]
+            for element in self.__model.predict(input_fn=val_input):
+                pred_val = np.append(pred_val, element["logits"])
 
-            pred_test = self.__net.predict(tes_feature_for_model).reshape((-1,))
-            self.__sub_preds += logit(pred_test) / self.__folds.n_splits
+            print("Fold %i prediction trn auc: %.5f" % (n_fold, roc_auc_score(val_y.tolist(), expit(pred_val.tolist()))))
+
+            for element in self.__model.predict(input_fn=tes_input):
+                pred_tes = np.append(pred_tes, element["logits"])
+
+            self.__val_preds[val_idx] += pred_val
+            self.__sub_preds += pred_tes / self.__folds.n_splits
 
             self.__columns_counts.clear()
-            del trn_x, val_x, tes_x, trn_y, val_y
+            del trn_x, val_x, tes_x, trn_y, val_y, self.__model
             gc.collect()
 
     def data_write(self):
-        print("Fold all prediction trn auc: %.5f" % (roc_auc_score(self.__train_label, self.__val_preds)))
+        print("Fold all prediction trn auc: %.5f" % (roc_auc_score(self.__train_label, expit(self.__val_preds))))
 
         self.__test_index["target"] = expit(self.__sub_preds.reshape((-1,)))
         self.__test_index.to_csv(os.path.join(self.__output_path, "sample_submission.csv"), index=False)
@@ -169,8 +167,8 @@ class WideAndDeepNeuralNet(object):
 
 if __name__ == "__main__":
     wadnn = WideAndDeepNeuralNet(
-        input_path="E:\\Kaggle\\Categorical Feature Encoding Challenge",
-        output_path="E:\\Kaggle\\Categorical Feature Encoding Challenge"
+        input_path="E:\\Kaggle\\Categorical_Feature_Encoding_Challenge",
+        output_path="E:\\Kaggle\\Categorical_Feature_Encoding_Challenge"
     )
     wadnn.data_read()
     wadnn.data_prepare()
